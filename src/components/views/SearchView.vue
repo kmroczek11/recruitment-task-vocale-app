bd2d49826fcf737570d004d1b1ade058490418f8
kmroczek11 <kamil.mroczek@onet.eu>
2024-11-22 16:58:53 +0100

    feature: added red border on input error

--- src/components/views/SearchView.vue
+++ src/components/views/SearchView.vue
@@ -4,8 +4,9 @@
       <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div class="mb-6">
           <input
-            class="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="array" type="text" placeholder="2, 4, 0, 100, 4, 11, 2602, 36" v-model="state.value">
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            :class="{ 'border-red-500': v$.value.$error }" id="array" type="text"
+            placeholder="2, 4, 0, 100, 4, 11, 2602, 36" v-model="state.value">
           <p class="text-red-500 text-xs italic" v-if="v$.value.$error">{{ v$.value.$errors[0].$message }}</p>
         </div>
         <div class="flex items-center justify-center">
